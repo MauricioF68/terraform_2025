d334c357c57ca89d4909a46d500df22e575a04ac
MauricioF68 <mauricioterrones98@gmail.com>
2025-09-14 23:17:14 -0500

    feat:Modificando el archivo y agregando los dos contenedores nginx (app2 y app3)

--- nginx.tf
+++ nginx.tf
@@ -1,6 +1,26 @@
-resource "docker_container" "nginx" {
-  name  = "nginx-${terraform.workspace}"
-  image = "nginx:stable-alpine3.21-perl"
+resource "docker_container" "app1" {
+  name  = "nginx-app1${terraform.workspace}"
+  image = "nginx:stable-perl"
+
+  ports {
+    internal = 80
+    external = var.nginx_external_port[terraform.workspace]
+  }
+}
+
+resource "docker_container" "app2" {
+  name  = "nginx-app2${terraform.workspace}"
+  image = "nginx:stable-perl"
+
+  ports {
+    internal = 80
+    external = var.nginx_external_port[terraform.workspace]
+  }
+}
+
+resource "docker_container" "app3" {
+  name  = "nginx-app3${terraform.workspace}"
+  image = "nginx:stable-perl"
 
   ports {
     internal = 80
